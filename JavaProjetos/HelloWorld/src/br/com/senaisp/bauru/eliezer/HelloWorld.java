f73aaa09d3ddcf26e174c010b4ad6350dfb83d62
senai-eliezer-sp <souzaeliezer578@gmail.com>
2024-05-11 12:18:38 -0300

    conteudo da aula 11-05-2024

---
+++ JavaProjetos/HelloWorld/src/br/com/senaisp/bauru/eliezer/HelloWorld.java
@@ -0,0 +1,10 @@
+package br.com.senaisp.bauru.eliezer;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Olá mundo!");
+	}
+
+}
